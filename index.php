4e2d021b9260262fc2ef306bec841f87392c914f
Maxence-schroeder01 <maxence.schroeder@gmail.com>
2021-12-24 01:11:45 +0000

    vue mobile et vue desktop de la navbar avec des diplay flex et animations sur la partie hamburgers

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://kit.fontawesome.com/2b4b8ea570.js" crossorigin="anonymous"></script>
+        <title>Maxence-Schroeder</title>
+</head>
+<body>
+<nav class="navbar fixed-top">
+  <div class="logo">
+      <h3>Maxence-Schroeder</h3>
+  </div>
+  <div class="burger">
+    <div class="line1"></div>
+    <div class="line2"></div>
+    <div class="line3"></div>
+  </div>
+  <ul class="nav-links ml-auto">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Portfolio</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+<nav>
+  <script src="js/navbar.js"></script>
+</body>
+</html>
