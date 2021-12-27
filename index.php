55d4a268188ee5eda77cf822f789ed3c8c862e1b
Maxence-schroeder01 <maxence.schroeder@gmail.com>
2021-12-27 00:29:09 +0000

    Commit du 26 dec 2021 avec commencement de #8 pour notament la partie config.php et preparation pour l'autre moitier de la page index #3

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
+<?php include_once 'config.php'; ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/style-navbar.css">
+        <link rel="stylesheet" href="css/style-general.css">
         <script src="https://kit.fontawesome.com/2b4b8ea570.js" crossorigin="anonymous"></script>
         <title>Maxence-Schroeder</title>
 </head>
@@ -24,7 +26,12 @@
     <li><a href="#">Portfolio</a></li>
     <li><a href="#">Contact</a></li>
   </ul>
-<nav>
+</nav>
+<section class="home-area element-cover-bg" style="background-image: url(img/home_page.jpg);">
+<div class="center">
+  <h1 class="home-name">Maxence Schroeder<h1>
+</div>
+</section>
   <script src="js/navbar.js"></script>
 </body>
 </html>
