6a9cc86db17bcec84b346c57242f277ad3d8f2ef
Maxence-schroeder01 <maxence.schroeder@gmail.com>
2022-01-01 22:08:25 +0000

    close #6 ajout de la page contact avec evoie d'email

--- pages/contact.php
+++ pages/contact.php
@@ -1,4 +1,5 @@
-<?php include_once 'config.php'; ?>
+<?php include_once 'config.php'; 
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../css/style-navbar.css">
+        <link rel="stylesheet" href="../css/style-contact.css">
         <link rel="stylesheet" href="../css/style-general.css">
         <script src="https://kit.fontawesome.com/2b4b8ea570.js" crossorigin="anonymous"></script>
         <title>Maxence-Schroeder</title>
@@ -27,16 +29,64 @@
     <li><a class="contact-s">Contact</a></li>
   </ul>
 </nav>
-<section class="home-area element-cover-bg" style="background-image: url(img/home_page.jpg);">
-<div class="center h-100">
-  <h1 class="home-name">Maxence Schroeder</h1>
-  <h3 class="cd-headline slide">
-      <span>Je suis</span>
-      <span class="cd-words-wrapper">
-         <b class="is-visible">Developpeur</b>
-         <b>Etudiant</b>
-         <b>Programmeur</b>
-</div>
+<section class="home-area element-cover-bg">
+
+<section id="contact">
+  
+  <h1 class="section-header">Contact</h1>
+  
+  <div class="contact-wrapper">
+  
+  <!-- Left contact page --> 
+    
+    <form id="contact-form" action="tools/send-mail.php" method="post" enctype="multipart/form-data" class="form-horizontal" role="form">
+       
+      <div class="form-group">
+        <div class="col-sm-12">
+          <input type="text" class="form-control" id="name" placeholder="NAME" name="name" value="" required>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="col-sm-12">
+          <input type="email" class="form-control" id="email" placeholder="EMAIL" name="email" value="" required>
+        </div>
+      </div>
+
+      <textarea class="form-control" rows="10" placeholder="MESSAGE" name="message" required></textarea>
+      
+      <button class="btn btn-primary send-button" id="submit" type="submit" value="SEND">
+        <div class="alt-send-button">
+          <i class="fa fa-paper-plane"></i><span class="send-text">SEND</span>
+        </div>
+      
+      </button>
+      
+    </form>
+    
+  <!-- Left contact page --> 
+    
+      <div class="direct-contact-container">
+
+      <hr>
+
+        <ul class="contact-list">
+          <li class="list-item"><i class="fas fa-map-marker fa-2x"><span class="contact-text place">Matane, Quebec</span></i></li>
+          
+          <li class="list-item"><i class="fas fa-phone fa-2x"><span class="contact-text phone"><a href="tel:+33613900037" title="Give me a call">+33613900037</a></span></i></li>
+          
+          <li class="list-item"><i class="fas fa-envelope fa-2x"><span class="contact-text gmail"><a href="mailto:maxence.schroeder@gmail.com" title="Send me an email">maxence.schroeder@gmail.com</a></span></i></li>
+          
+        </ul>
+        <hr>
+      </div>
+    
+  </div>
+  
+</section>  
+  
+  
+
 <div class="fixed-block blockright">
   <ul class="social-icons">
     <li><a href="https://github.com/Maxence-schroeder01" aria-label="Github"><i class="fa fa-github fa-2x"></i></a></li>
@@ -55,6 +105,7 @@
 <script src="../js/jquery-2.1.1.js"></script>
 <script src="../js/navbar.js"></script>
 <script src="../js/transition.js"></script>
+<script src="https://smtpjs.com/v3/smtp.js"></script>
 <script src="../js/home-text.js"></script>
 </body>
 </html>
