cae7d7cc7065bc3b418b562725441a1f7902aa70
Maxence-schroeder01 <maxence.schroeder@gmail.com>
2022-01-19 20:32:17 +0000

    LastPush Before host on my https vps and add somme big correctif

--- pages/about.php
+++ pages/about.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../css/style-navbar.css">
+        <link rel="stylesheet" href="../css/style-about.css">
         <link rel="stylesheet" href="../css/style-general.css">
         <script src="https://kit.fontawesome.com/2b4b8ea570.js" crossorigin="anonymous"></script>
         <title>Maxence-Schroeder</title>
@@ -27,16 +28,35 @@
     <li><a class="contact-s">Contact</a></li>
   </ul>
 </nav>
-<section class="home-area element-cover-bg" style="background-image: url(img/home_page.jpg);">
-<div class="center h-100">
-  <h1 class="home-name">Maxence Schroeder</h1>
-  <h3 class="cd-headline slide">
-      <span>Je suis</span>
-      <span class="cd-words-wrapper">
-         <b class="is-visible">Developpeur</b>
-         <b>Etudiant</b>
-         <b>Programmeur</b>
-</div>
+<section class="home-area element-cover-bg">
+<div class="container">
+    <section class="grid info">
+      <div class="column-xs-12 column-md-1">
+        <div class="about">
+        <h1 class="section-heading">About Me</h1>
+        </div>
+      </div>
+      <div class="column-xs-12 column-md-4">
+        <img src="../img/home_profile.jpg">
+      </div>
+      <div class="column-xs-12 column-md-7">
+        <div class="intro">
+          <h2>Jane Writer is an award-winning author and lifestyle blogger.</h2>
+          <p>Crud indignant permissively through burped nodded timorous onto the as wore less ouch far rewound considering so broken dachshund jeepers sanely confident. Contemplated growled apart enthusiastically punitively much much darn onto deep dear returned some cockatoo hungrily fortuitously enchantingly ouch sanely on unceremonious especially much yikes darn.</p>
+        </div>
+      </div>
+    </section>
+  </div>
+</main>
+<footer>
+  <div class="container">
+    <section class="grid">
+      <div class="column-xs-12">
+        <p class="copyright">&copy; Copyright 2018 Katherine Kato</p>
+      </div>
+    </section>
+  </div>
+</footer>
 <div class="fixed-block blockright">
   <ul class="social-icons">
     <li><a href="https://github.com/Maxence-schroeder01" aria-label="Github"><i class="fa fa-github fa-2x"></i></a></li>
